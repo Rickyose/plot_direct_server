2e3894d9869d7716779fb9874b0913ecb50a6938
Rickyose <rickyhaterearged@gmail.com>
2023-05-25 22:11:10 +0700

    Update vncsetup_plot.sh

--- vncsetup_plot.sh
+++ vncsetup_plot.sh
@@ -6,6 +6,7 @@
 cd && cd /root/
 sudo apt-get update -y
 sudo apt-get install gnome-shell -y && sudo apt-get install ubuntu-gnome-desktop -y && sudo apt-get install autocutsel -y && sudo apt-get install gnome-core -y && sudo apt-get install gnome-panel -y && sudo apt-get install gnome-themes-standard -y && sudo apt install -y iotop libsodium-dev libgmp3-dev cmake g++ git -y
+sudo apt install -y libsodium-dev cmake g++ git build-essential
 git clone https://github.com/madMAx43v3r/chia-plotter.git && cd chia-plotter
 git submodule update --init && ./make_devel.sh && ./build/chia_plot --help
 cd /root/

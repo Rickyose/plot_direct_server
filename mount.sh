86332fc1aa54d3edf974c3bd2207fed39f8dad9d
Rickyose <rickyhaterearged@gmail.com>
2023-05-25 19:31:09 +0700

    Update mount.sh

--- mount.sh
+++ mount.sh
@@ -3,93 +3,93 @@
 
 cd &&  cd /
 mount /dev/nvme0n1 /plot1 && mount /dev/sdb /plot3
-sudo -u root chown -R ubuntu /gdrive1
-sudo -u root chown -R ubuntu /gdrive2
-sudo -u root chown -R ubuntu /gdrive3
-sudo -u root chown -R ubuntu /gdrive4
-sudo -u root chown -R ubuntu /gdrive5
-sudo -u root chown -R ubuntu /gdrive6
-sudo -u root chown -R ubuntu /gdrive7
-sudo -u root chown -R ubuntu /gdrive8
-sudo -u root chown -R ubuntu /gdrive9
-sudo -u root chown -R ubuntu /gdrive10
-sudo -u root chown -R ubuntu /gdrive11
-sudo -u root chown -R ubuntu /gdrive12
-sudo -u root chown -R ubuntu /gdrive13
-sudo -u root chown -R ubuntu /gdrive14
-sudo -u root chown -R ubuntu /gdrive15
-sudo -u root chown -R ubuntu /gdrive16
-sudo -u root chown -R ubuntu /gdrive17
-sudo -u root chown -R ubuntu /gdrive18
-sudo -u root chown -R ubuntu /gdrive19
-sudo -u root chown -R ubuntu /gdrive20
-sudo -u root chown -R ubuntu /gdrive21
-sudo -u root chown -R ubuntu /gdrive22
-sudo -u root chown -R ubuntu /gdrive23
-sudo -u root chown -R ubuntu /gdrive24
-sudo -u root chown -R ubuntu /gdrive25
-sudo -u root chown -R ubuntu /gdrive26
-sudo -u root chown -R ubuntu /gdrive27
-sudo -u root chown -R ubuntu /gdrive28
-sudo -u root chown -R ubuntu /gdrive29
-sudo -u root chown -R ubuntu /gdrive30
-sudo -u root chown -R ubuntu /gdrive31
-sudo -u root chown -R ubuntu /gdrive32
-sudo -u root chown -R ubuntu /gdrive33
-sudo -u root chown -R ubuntu /gdrive34
-sudo -u root chown -R ubuntu /gdrive35
-sudo -u root chown -R ubuntu /gdrive36
-sudo -u root chown -R ubuntu /gdrive37
-sudo -u root chown -R ubuntu /gdrive38
-sudo -u root chown -R ubuntu /gdrive39
-sudo -u root chown -R ubuntu /gdrive40
-sudo -u root chown -R ubuntu /gdrive41
-sudo -u root chown -R ubuntu /gdrive42
-sudo -u root chown -R ubuntu /gdrive43
-sudo -u root chown -R ubuntu /gdrive44
-sudo -u root chown -R ubuntu /gdrive45
-sudo -u root chown -R ubuntu /gdrive46
-sudo -u root chown -R ubuntu /gdrive47
-sudo -u root chown -R ubuntu /gdrive48
-sudo -u root chown -R ubuntu /gdrive49
-sudo -u root chown -R ubuntu /gdrive50
-sudo -u ubuntu rclone mount --allow-non-empty --daemon gdrive1: /gdrive1 && chmod 777 gdrive1 && chown -R ubuntu /gdrive1 & sleep 5 
-sudo -u ubuntu rclone mount --allow-non-empty --daemon gdrive2: /gdrive2 && chmod 777 gdrive2 && chown -R ubuntu /gdrive2 & sleep 5
-sudo -u ubuntu rclone mount --allow-non-empty --daemon gdrive3: /gdrive3 && chmod 777 gdrive3 && chown -R ubuntu /gdrive3 & sleep 5 
-sudo -u ubuntu rclone mount --allow-non-empty --daemon gdrive4: /gdrive4 && chmod 777 gdrive4 && chown -R ubuntu /gdrive4 & sleep 5 
-sudo -u ubuntu rclone mount --allow-non-empty --daemon gdrive5: /gdrive5 && chmod 777 gdrive5 && chown -R ubuntu /gdrive5 & sleep 5
-sudo -u ubuntu rclone mount --allow-non-empty --daemon gdrive6: /gdrive6 && chmod 777 gdrive6 && chown -R ubuntu /gdrive6 & sleep 5 
-sudo -u ubuntu rclone mount --allow-non-empty --daemon gdrive7: /gdrive7 && chmod 777 gdrive7 && chown -R ubuntu /gdrive7 & sleep 5 
-sudo -u ubuntu rclone mount --allow-non-empty --daemon gdrive8: /gdrive8 && chmod 777 gdrive8 && chown -R ubuntu /gdrive8 & sleep 5
-sudo -u ubuntu rclone mount --allow-non-empty --daemon gdrive9: /gdrive9 && chmod 777 gdrive9 && chown -R ubuntu /gdrive9 & sleep 5
-sudo -u ubuntu rclone mount --allow-non-empty --daemon gdrive10: /gdrive10 && chmod 777 gdrive10 && chown -R ubuntu /gdrive10 & sleep 5
-sudo -u ubuntu rclone mount --allow-non-empty --daemon gdrive11: /gdrive11 && chmod 777 gdrive11 && chown -R ubuntu /gdrive11 & sleep 5 
-sudo -u ubuntu rclone mount --allow-non-empty --daemon gdrive12: /gdrive12 && chmod 777 gdrive12 && chown -R ubuntu /gdrive12 & sleep 5 
-sudo -u ubuntu rclone mount --allow-non-empty --daemon gdrive13: /gdrive13 && chmod 777 gdrive13 && chown -R ubuntu /gdrive13 & sleep 5 
-sudo -u ubuntu rclone mount --allow-non-empty --daemon gdrive14: /gdrive14 && chmod 777 gdrive14 && chown -R ubuntu /gdrive14 & sleep 5
-sudo -u ubuntu rclone mount --allow-non-empty --daemon gdrive15: /gdrive15 && chmod 777 gdrive15 && chown -R ubuntu /gdrive15 & sleep 5
-sudo -u ubuntu rclone mount --allow-non-empty --daemon gdrive16: /gdrive16 && chmod 777 gdrive16 && chown -R ubuntu /gdrive16 & sleep 5
-sudo -u ubuntu rclone mount --allow-non-empty --daemon gdrive17: /gdrive17 && chmod 777 gdrive17 && chown -R ubuntu /gdrive17 & sleep 5 
-sudo -u ubuntu rclone mount --allow-non-empty --daemon gdrive18: /gdrive18 && chmod 777 gdrive18 && chown -R ubuntu /gdrive18 & sleep 5 
-sudo -u ubuntu rclone mount --allow-non-empty --daemon gdrive19: /gdrive19 && chmod 777 gdrive19 && chown -R ubuntu /gdrive19 & sleep 5
-sudo -u ubuntu rclone mount --allow-non-empty --daemon gdrive20: /gdrive20 && chmod 777 gdrive20 && chown -R ubuntu /gdrive20 & sleep 5
-sudo -u ubuntu rclone mount --allow-non-empty --daemon gdrive31: /gdrive31 && chmod 777 gdrive31 && chown -R ubuntu /gdrive31 & sleep 5 
-sudo -u ubuntu rclone mount --allow-non-empty --daemon gdrive32: /gdrive32 && chmod 777 gdrive32 && chown -R ubuntu /gdrive32 & sleep 5
-sudo -u ubuntu rclone mount --allow-non-empty --daemon gdrive33: /gdrive33 && chmod 777 gdrive33 && chown -R ubuntu /gdrive33 & sleep 5 
-sudo -u ubuntu rclone mount --allow-non-empty --daemon gdrive34: /gdrive34 && chmod 777 gdrive34 && chown -R ubuntu /gdrive34 & sleep 5 
-sudo -u ubuntu rclone mount --allow-non-empty --daemon gdrive35: /gdrive35 && chmod 777 gdrive35 && chown -R ubuntu /gdrive35 & sleep 5
-sudo -u ubuntu rclone mount --allow-non-empty --daemon gdrive36: /gdrive36 && chmod 777 gdrive36 && chown -R ubuntu /gdrive36 & sleep 5 
-sudo -u ubuntu rclone mount --allow-non-empty --daemon gdrive37: /gdrive37 && chmod 777 gdrive37 && chown -R ubuntu /gdrive37 & sleep 5 
-sudo -u ubuntu rclone mount --allow-non-empty --daemon gdrive38: /gdrive38 && chmod 777 gdrive38 && chown -R ubuntu /gdrive38 & sleep 5
-sudo -u ubuntu rclone mount --allow-non-empty --daemon gdrive39: /gdrive39 && chmod 777 gdrive39 && chown -R ubuntu /gdrive39 & sleep 5
-sudo -u ubuntu rclone mount --allow-non-empty --daemon gdrive40: /gdrive40 && chmod 777 gdrive40 && chown -R ubuntu /gdrive40 & sleep 5
-sudo -u ubuntu rclone mount --allow-non-empty --daemon gdrive41: /gdrive41 && chmod 777 gdrive41 && chown -R ubuntu /gdrive41 & sleep 5 
-sudo -u ubuntu rclone mount --allow-non-empty --daemon gdrive42: /gdrive42 && chmod 777 gdrive42 && chown -R ubuntu /gdrive42 & sleep 5 
-sudo -u ubuntu rclone mount --allow-non-empty --daemon gdrive43: /gdrive43 && chmod 777 gdrive43 && chown -R ubuntu /gdrive43 & sleep 5 
-sudo -u ubuntu rclone mount --allow-non-empty --daemon gdrive44: /gdrive44 && chmod 777 gdrive44 && chown -R ubuntu /gdrive44 & sleep 5
-sudo -u ubuntu rclone mount --allow-non-empty --daemon gdrive45: /gdrive45 && chmod 777 gdrive45 && chown -R ubuntu /gdrive45 & sleep 5
-sudo -u ubuntu rclone mount --allow-non-empty --daemon gdrive46: /gdrive46 && chmod 777 gdrive46 && chown -R ubuntu /gdrive46 & sleep 5
-sudo -u ubuntu rclone mount --allow-non-empty --daemon gdrive47: /gdrive47 && chmod 777 gdrive47 && chown -R ubuntu /gdrive47 & sleep 5 
-sudo -u ubuntu rclone mount --allow-non-empty --daemon gdrive48: /gdrive48 && chmod 777 gdrive48 && chown -R ubuntu /gdrive48 & sleep 5 
-sudo -u ubuntu rclone mount --allow-non-empty --daemon gdrive49: /gdrive49 && chmod 777 gdrive49 && chown -R ubuntu /gdrive49 & sleep 5
-sudo -u ubuntu rclone mount --allow-non-empty --daemon gdrive50: /gdrive50 && chmod 777 gdrive50 && chown -R ubuntu /gdrive50 & sleep 5
+sudo -u root chown -R root /gdrive1
+sudo -u root chown -R root /gdrive2
+sudo -u root chown -R root /gdrive3
+sudo -u root chown -R root /gdrive4
+sudo -u root chown -R root /gdrive5
+sudo -u root chown -R root /gdrive6
+sudo -u root chown -R root /gdrive7
+sudo -u root chown -R root /gdrive8
+sudo -u root chown -R root /gdrive9
+sudo -u root chown -R root /gdrive10
+sudo -u root chown -R root /gdrive11
+sudo -u root chown -R root /gdrive12
+sudo -u root chown -R root /gdrive13
+sudo -u root chown -R root /gdrive14
+sudo -u root chown -R root /gdrive15
+sudo -u root chown -R root /gdrive16
+sudo -u root chown -R root /gdrive17
+sudo -u root chown -R root /gdrive18
+sudo -u root chown -R root /gdrive19
+sudo -u root chown -R root /gdrive20
+sudo -u root chown -R root /gdrive21
+sudo -u root chown -R root /gdrive22
+sudo -u root chown -R root /gdrive23
+sudo -u root chown -R root /gdrive24
+sudo -u root chown -R root /gdrive25
+sudo -u root chown -R root /gdrive26
+sudo -u root chown -R root /gdrive27
+sudo -u root chown -R root /gdrive28
+sudo -u root chown -R root /gdrive29
+sudo -u root chown -R root /gdrive30
+sudo -u root chown -R root /gdrive31
+sudo -u root chown -R root /gdrive32
+sudo -u root chown -R root /gdrive33
+sudo -u root chown -R root /gdrive34
+sudo -u root chown -R root /gdrive35
+sudo -u root chown -R root /gdrive36
+sudo -u root chown -R root /gdrive37
+sudo -u root chown -R root /gdrive38
+sudo -u root chown -R root /gdrive39
+sudo -u root chown -R root /gdrive40
+sudo -u root chown -R root /gdrive41
+sudo -u root chown -R root /gdrive42
+sudo -u root chown -R root /gdrive43
+sudo -u root chown -R root /gdrive44
+sudo -u root chown -R root /gdrive45
+sudo -u root chown -R root /gdrive46
+sudo -u root chown -R root /gdrive47
+sudo -u root chown -R root /gdrive48
+sudo -u root chown -R root /gdrive49
+sudo -u root chown -R root /gdrive50
+sudo -u root rclone mount --allow-non-empty --daemon gdrive1: /gdrive1 && chmod 777 /gdrive1 && chown -R root /gdrive1 & sleep 5 
+sudo -u root rclone mount --allow-non-empty --daemon gdrive2: /gdrive2 && chmod 777 /gdrive2 && chown -R root /gdrive2 & sleep 5
+sudo -u root rclone mount --allow-non-empty --daemon gdrive3: /gdrive3 && chmod 777 /gdrive3 && chown -R root /gdrive3 & sleep 5 
+sudo -u root rclone mount --allow-non-empty --daemon gdrive4: /gdrive4 && chmod 777 /gdrive4 && chown -R root /gdrive4 & sleep 5 
+sudo -u root rclone mount --allow-non-empty --daemon gdrive5: /gdrive5 && chmod 777 /gdrive5 && chown -R root /gdrive5 & sleep 5
+sudo -u root rclone mount --allow-non-empty --daemon gdrive6: /gdrive6 && chmod 777 /gdrive6 && chown -R root /gdrive6 & sleep 5 
+sudo -u root rclone mount --allow-non-empty --daemon gdrive7: /gdrive7 && chmod 777 /gdrive7 && chown -R root /gdrive7 & sleep 5 
+sudo -u root rclone mount --allow-non-empty --daemon gdrive8: /gdrive8 && chmod 777 /gdrive8 && chown -R root /gdrive8 & sleep 5
+sudo -u root rclone mount --allow-non-empty --daemon gdrive9: /gdrive9 && chmod 777 /gdrive9 && chown -R root /gdrive9 & sleep 5
+sudo -u root rclone mount --allow-non-empty --daemon gdrive10: /gdrive10 && chmod 777 /gdrive10 && chown -R root /gdrive10 & sleep 5
+sudo -u root rclone mount --allow-non-empty --daemon gdrive11: /gdrive11 && chmod 777 /gdrive11 && chown -R root /gdrive11 & sleep 5 
+sudo -u root rclone mount --allow-non-empty --daemon gdrive12: /gdrive12 && chmod 777 /gdrive12 && chown -R root /gdrive12 & sleep 5 
+sudo -u root rclone mount --allow-non-empty --daemon gdrive13: /gdrive13 && chmod 777 /gdrive13 && chown -R root /gdrive13 & sleep 5 
+sudo -u root rclone mount --allow-non-empty --daemon gdrive14: /gdrive14 && chmod 777 /gdrive14 && chown -R root /gdrive14 & sleep 5
+sudo -u root rclone mount --allow-non-empty --daemon gdrive15: /gdrive15 && chmod 777 /gdrive15 && chown -R root /gdrive15 & sleep 5
+sudo -u root rclone mount --allow-non-empty --daemon gdrive16: /gdrive16 && chmod 777 /gdrive16 && chown -R root /gdrive16 & sleep 5
+sudo -u root rclone mount --allow-non-empty --daemon gdrive17: /gdrive17 && chmod 777 /gdrive17 && chown -R root /gdrive17 & sleep 5 
+sudo -u root rclone mount --allow-non-empty --daemon gdrive18: /gdrive18 && chmod 777 /gdrive18 && chown -R root /gdrive18 & sleep 5 
+sudo -u root rclone mount --allow-non-empty --daemon gdrive19: /gdrive19 && chmod 777 /gdrive19 && chown -R root /gdrive19 & sleep 5
+sudo -u root rclone mount --allow-non-empty --daemon gdrive20: /gdrive20 && chmod 777 /gdrive20 && chown -R root /gdrive20 & sleep 5
+sudo -u root rclone mount --allow-non-empty --daemon gdrive31: /gdrive31 && chmod 777 /gdrive31 && chown -R root /gdrive31 & sleep 5 
+sudo -u root rclone mount --allow-non-empty --daemon gdrive32: /gdrive32 && chmod 777 /gdrive32 && chown -R root /gdrive32 & sleep 5
+sudo -u root rclone mount --allow-non-empty --daemon gdrive33: /gdrive33 && chmod 777 /gdrive33 && chown -R root /gdrive33 & sleep 5 
+sudo -u root rclone mount --allow-non-empty --daemon gdrive34: /gdrive34 && chmod 777 /gdrive34 && chown -R root /gdrive34 & sleep 5 
+sudo -u root rclone mount --allow-non-empty --daemon gdrive35: /gdrive35 && chmod 777 /gdrive35 && chown -R root /gdrive35 & sleep 5
+sudo -u root rclone mount --allow-non-empty --daemon gdrive36: /gdrive36 && chmod 777 /gdrive36 && chown -R root /gdrive36 & sleep 5 
+sudo -u root rclone mount --allow-non-empty --daemon gdrive37: /gdrive37 && chmod 777 /gdrive37 && chown -R root /gdrive37 & sleep 5 
+sudo -u root rclone mount --allow-non-empty --daemon gdrive38: /gdrive38 && chmod 777 /gdrive38 && chown -R root /gdrive38 & sleep 5
+sudo -u root rclone mount --allow-non-empty --daemon gdrive39: /gdrive39 && chmod 777 /gdrive39 && chown -R root /gdrive39 & sleep 5
+sudo -u root rclone mount --allow-non-empty --daemon gdrive40: /gdrive40 && chmod 777 /gdrive40 && chown -R root /gdrive40 & sleep 5
+sudo -u root rclone mount --allow-non-empty --daemon gdrive41: /gdrive41 && chmod 777 /gdrive41 && chown -R root /gdrive41 & sleep 5 
+sudo -u root rclone mount --allow-non-empty --daemon gdrive42: /gdrive42 && chmod 777 /gdrive42 && chown -R root /gdrive42 & sleep 5 
+sudo -u root rclone mount --allow-non-empty --daemon gdrive43: /gdrive43 && chmod 777 /gdrive43 && chown -R root /gdrive43 & sleep 5 
+sudo -u root rclone mount --allow-non-empty --daemon gdrive44: /gdrive44 && chmod 777 /gdrive44 && chown -R root /gdrive44 & sleep 5
+sudo -u root rclone mount --allow-non-empty --daemon gdrive45: /gdrive45 && chmod 777 /gdrive45 && chown -R root /gdrive45 & sleep 5
+sudo -u root rclone mount --allow-non-empty --daemon gdrive46: /gdrive46 && chmod 777 /gdrive46 && chown -R root /gdrive46 & sleep 5
+sudo -u root rclone mount --allow-non-empty --daemon gdrive47: /gdrive47 && chmod 777 /gdrive47 && chown -R root /gdrive47 & sleep 5 
+sudo -u root rclone mount --allow-non-empty --daemon gdrive48: /gdrive48 && chmod 777 /gdrive48 && chown -R root /gdrive48 & sleep 5 
+sudo -u root rclone mount --allow-non-empty --daemon gdrive49: /gdrive49 && chmod 777 /gdrive49 && chown -R root /gdrive49 & sleep 5
+sudo -u root rclone mount --allow-non-empty --daemon gdrive50: /gdrive50 && chmod 777 /gdrive50 && chown -R root /gdrive50 & sleep 5

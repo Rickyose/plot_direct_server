5eeef9d426fdc6344410075a073f9331a30d1188
Rickyose <rickyhaterearged@gmail.com>
2023-05-25 20:47:13 +0700

    Update plot_start.sh

--- plot_start.sh
+++ plot_start.sh
@@ -17,8 +17,8 @@ export ftp_1=0
 export mount_gdrive3=`df -h | grep gdrive3 | wc -l`
 
 ###########################################################################
-sudo mkdir /plot1 && sudo chown -R ubuntu /plot1
-sudo mkdir /plot1/zip_plot && sudo chown -R ubuntu /plot1/zip_plot
+sudo mkdir /plot1 && sudo chown -R root /plot1
+sudo mkdir /plot1/zip_plot && sudo chown -R root /plot1/zip_plot
 cd 
 find /plot1/ -maxdepth 1 -type f -delete
 find /plot3/ -maxdepth 1 -type f -delete
@@ -38,8 +38,8 @@ if [ $initiate_start -eq 1 ]; then
 		rm -rf skripburu2
 		git clone https://github.com/Rickyose/skripburu2
 		sleep 30
-		chmod +x /home/ubuntu/skripburu2/buru2.sh
-		/home/ubuntu/skripburu2/buru2.sh 
+		chmod +x /root/skripburu2/buru2.sh
+		/root/skripburu2/buru2.sh 
 		sleep 3600
 	done
 fi
@@ -51,7 +51,7 @@ if [ $initiate_start -eq 1 ]; then
 	if [ 100 -gt 1 ]; then
 		ip=`curl ifconfig.me`
 		over_capacity_2=`find /plot1/ -name "*.plot" | wc -l`
-		if [ $over_capacity_2 -ge 2 ]; then
+		if [ $over_capacity_2 -ge 1 ]; then
 		no_storage_left="NO STORAGE IN VPS"
 		else
 		no_storage_left="MASIH ADA STORAGE"
@@ -62,20 +62,20 @@ if [ $initiate_start -eq 1 ]; then
 		JUMLAH PLOT SEMUA = $new_total_plot 
 		OVER KAPASITAS = $no_storage_left "
 
-		mkdir /home/ubuntu/Documents/
+		mkdir /root/Documents/
 		sleep 5
-		rm -rf /home/ubuntu/Documents/"$ip".json
+		rm -rf /root/Documents/"$ip".json
 		while [ `ls | grep "$ip".json | wc -l` -gt 0 ]
 		do
 			sleep 5
-			rm -rf /home/ubuntu/Documents/"$ip".json
+			rm -rf /root/Documents/"$ip".json
 		done
 		echo "$multi_line"
-		echo "$multi_line" > /home/ubuntu/Documents/"$ip".json
+		echo "$multi_line" > /root/Documents/"$ip".json
 		if [ `ps ux | grep "python3 -m http.server" | grep -v "grep" | awk '{print $2}'` -gt 0 ]; then
 			sleep 1
 		else
-			python3 -m http.server --directory /home/ubuntu/Documents/
+			python3 -m http.server --directory /root/Documents/
 		fi
 	fi
 fi
@@ -96,13 +96,15 @@ fi
 if [ $initiate_start -eq 1 ]; then
 	plot_number=0
 	sudo fstrim -v /plot1
-	sudo -u ubuntu rclone mount --allow-non-empty --daemon gdrive1: /gdrive1 && chmod 777 /gdrive1 && chown -R ubuntu /gdrive1 
+	sudo -u root rclone mount --allow-non-empty --daemon gdrive1: /gdrive1 && chmod 777 /gdrive1 && chown -R root /gdrive1 
 	mkdir /gdrive1/cha/
-	sudo -u ubuntu rclone mount --allow-non-empty --daemon gdrive2: /gdrive2 && chmod 777 /gdrive2 && chown -R ubuntu /gdrive2
+	sudo -u root rclone mount --allow-non-empty --daemon gdrive2: /gdrive2 && chmod 777 /gdrive2 && chown -R root /gdrive2
 	mkdir /gdrive2/cha/
+	sudo -u root rclone mount --allow-non-empty --daemon gdrive3: /gdrive2 && chmod 777 /gdrive3 && chown -R root /gdrive3
+	mkdir /gdrive3/cha/
 	while [ 1 -gt 0 ]
 	do
-		if [ $plot_number -eq 2 ]; then
+		if [ $plot_number -eq 3 ]; then
 			sudo fstrim -v /plot1
 			plot_number=0
 		fi
@@ -115,18 +117,19 @@ if [ $initiate_start -eq 1 ]; then
 				if [ `ls /plot1 | wc -l` -eq 2 ]; then
 					ls -t | tail -1 | xargs rm -f
 				fi
-				head -c 1000000 </dev/urandom > /home/ubuntu/gdrive_daily_limit.txt && mv -f /home/ubuntu/gdrive_daily_limit.txt /gdrive1/cha/
+				head -c 1000000 </dev/urandom > /root/gdrive_daily_limit.txt && mv -f /root/gdrive_daily_limit.txt /gdrive1/cha/
 				if [ `wc -c /gdrive1/cha/gdrive_daily_limit.txt | awk '{print $1}'` -gt 0 ]; then
 					# Ini -u 512 dan -v 128 diubah jadi -u 128 dan tidak pake -v, coba dilihat effisiensi harga VM/day nya dengan kecepatan plotnya jika lebih efisien jangan dirubah
-					/home/ubuntu/chia-plotter/build/chia_plot_k34 --rmulti2 2 -n 1 -r 4 -u 128 -k 33 -t /plot1/ -d /gdrive1/cha/ -c xch1gk8wc8amphzn3jdf7ej8kp9qnxljnvza5ehum7j2tpne57qmvqws2rzjyn -f b4a93c3c283f771f8f108ed42e10431349238cd8a02654c55bb48b25d689e7c203f63243c7dcc37cb7a56f24a36cd976 &
-					rm -rf /home/ubuntu/gdrive_daily_limit.txt
+					/root/chia-plotter/build/chia_plot_k34 --rmulti2 2 -n 1 -r 4 -u 128 -k 33 -t /plot1/ -d /gdrive1/cha/ -c xch1gk8wc8amphzn3jdf7ej8kp9qnxljnvza5ehum7j2tpne57qmvqws2rzjyn -f b4a93c3c283f771f8f108ed42e10431349238cd8a02654c55bb48b25d689e7c203f63243c7dcc37cb7a56f24a36cd976 &
+					rm -rf /root/gdrive_daily_limit.txt
 					rm -rf /gdrive1/cha/gdrive_daily_limit.txt
 					plot_number=1
 				else
 					echo "GOOGLE OVER DAILY LIMIT PADA GDRIVE 1"
 					sleep 10s
-					rm -rf /home/ubuntu/gdrive_daily_limit.txt
+					rm -rf /root/gdrive_daily_limit.txt
 					rm -rf /gdrive1/cha/gdrive_daily_limit.txt
+					plot_number=1
 				fi
 				
 			fi
@@ -140,18 +143,45 @@ if [ $initiate_start -eq 1 ]; then
 				if [ `ls /plot1 | wc -l` -eq 2 ]; then
 					ls -t | tail -1 | xargs rm -f
 				fi
-				head -c 1000000 </dev/urandom > /home/ubuntu/gdrive_daily_limit.txt && mv -f /home/ubuntu/gdrive_daily_limit.txt /gdrive2/cha/
+				head -c 1000000 </dev/urandom > /root/gdrive_daily_limit.txt && mv -f /root/gdrive_daily_limit.txt /gdrive2/cha/
 				if [ `wc -c /gdrive2/cha/gdrive_daily_limit.txt | awk '{print $1}'` -gt 0 ]; then
 					# Ini -u 512 dan -v 128 diubah jadi -u 128 dan tidak pake -v, coba dilihat effisiensi harga VM/day nya dengan kecepatan plotnya jika lebih efisien jangan dirubah
-					/home/ubuntu/chia-plotter/build/chia_plot_k34 --rmulti2 2 -n 1 -r 4 -u 128 -k 33 -t /plot1/ -d /gdrive2/cha/ -c xch1gk8wc8amphzn3jdf7ej8kp9qnxljnvza5ehum7j2tpne57qmvqws2rzjyn -f b4a93c3c283f771f8f108ed42e10431349238cd8a02654c55bb48b25d689e7c203f63243c7dcc37cb7a56f24a36cd976 &
-					rm -rf /home/ubuntu/gdrive_daily_limit.txt
+					/root/chia-plotter/build/chia_plot_k34 --rmulti2 2 -n 1 -r 4 -u 128 -k 33 -t /plot1/ -d /gdrive2/cha/ -c xch1gk8wc8amphzn3jdf7ej8kp9qnxljnvza5ehum7j2tpne57qmvqws2rzjyn -f b4a93c3c283f771f8f108ed42e10431349238cd8a02654c55bb48b25d689e7c203f63243c7dcc37cb7a56f24a36cd976 &
+					rm -rf /root/gdrive_daily_limit.txt
 					rm -rf /gdrive2/cha/gdrive_daily_limit.txt
 					plot_number=2
 				else
 					echo "GOOGLE OVER DAILY LIMIT PADA GDRIVE 2"
 					sleep 10s
-					rm -rf /home/ubuntu/gdrive_daily_limit.txt
+					rm -rf /root/gdrive_daily_limit.txt
 					rm -rf /gdrive2/cha/gdrive_daily_limit.txt
+					plot_number=2
+				fi
+				
+			fi
+		done
+		sleep 30s
+		echo "PLOT Number 3"
+		while [ $plot_number -eq 2 ]
+		do
+			sleep 10s
+			if [ `ls /plot1 | wc -l` -le 2 ]; then
+				if [ `ls /plot1 | wc -l` -eq 2 ]; then
+					ls -t | tail -1 | xargs rm -f
+				fi
+				head -c 1000000 </dev/urandom > /root/gdrive_daily_limit.txt && mv -f /root/gdrive_daily_limit.txt /gdrive3/cha/
+				if [ `wc -c /gdrive3/cha/gdrive_daily_limit.txt | awk '{print $1}'` -gt 0 ]; then
+					# Ini -u 512 dan -v 128 diubah jadi -u 128 dan tidak pake -v, coba dilihat effisiensi harga VM/day nya dengan kecepatan plotnya jika lebih efisien jangan dirubah
+					/root/chia-plotter/build/chia_plot_k34 --rmulti2 2 -n 1 -r 4 -u 128 -k 33 -t /plot1/ -d /gdrive3/cha/ -c xch1gk8wc8amphzn3jdf7ej8kp9qnxljnvza5ehum7j2tpne57qmvqws2rzjyn -f b4a93c3c283f771f8f108ed42e10431349238cd8a02654c55bb48b25d689e7c203f63243c7dcc37cb7a56f24a36cd976 &
+					rm -rf /root/gdrive_daily_limit.txt
+					rm -rf /gdrive2/cha/gdrive_daily_limit.txt
+					plot_number=3
+				else
+					echo "GOOGLE OVER DAILY LIMIT PADA GDRIVE 2"
+					sleep 10s
+					rm -rf /root/gdrive_daily_limit.txt
+					rm -rf /gdrive2/cha/gdrive_daily_limit.txt
+					plot_number=3
 				fi
 				
 			fi
@@ -160,7 +190,8 @@ if [ $initiate_start -eq 1 ]; then
 		####################
 		 gdrive1_total_plot=`find /gdrive1/cha/ -type f -size +200G -printf 1 | wc -c`
 		 gdrive2_total_plot=`find /gdrive2/cha/ -type f -size +200G -printf 1 | wc -c`
-		 new_total_plot=$((($gdrive1_total_plot + $gdrive2_total_plot)*1))
+		 gdrive3_total_plot=`find /gdrive3/cha/ -type f -size +200G -printf 1 | wc -c`
+		 new_total_plot=$((($gdrive1_total_plot + $gdrive2_total_plot + $gdrive3_total_plot)*1))
 		 echo "KIRIM PESAN KE ftp"
 		 ftp_script &
 		
